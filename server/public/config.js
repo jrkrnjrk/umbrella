6846f63802681fdc5da89b88350287f4ba1eacfa
jrkrnjrk <154704804+jrkrnjrk@users.noreply.github.com>
2026-09-19 20:14:31 +0200

    Delete server directory

--- server/public/config.js
+++
@@ -1,6 +0,0 @@
-/* Edit these after you create the Discord application and Railway service. */
-window.UMBRELLA_CONFIG = {
-  clientId: "YOUR_DISCORD_CLIENT_ID",
-  permissions: "268520448",
-  marketingName: "Umbrella Verification",
-};
